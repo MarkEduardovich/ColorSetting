6de440335becf2bca4ed815e80e3a8e668f6e986
MarkEduardovich <321yamaha123@gmail.com>
2023-02-02 20:41:28 +0200

    finishCommit

--- colorSetting/ViewController.swift
+++ colorSetting/ViewController.swift
@@ -24,18 +24,44 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         colorView.layer.cornerRadius = 10
-
-
+        
+        redSlider.tintColor = .red
+        redSlider.value = 0.5
+        greenSlider.tintColor = .green
+        greenSlider.value = 0.5
+        blueSlider.tintColor = .blue
+        blueSlider.value = 0.5
+        
+        redValue.text = String(redSlider.value)
+        greenValue.text = String(greenSlider.value)
+        blueValue.text = String(blueSlider.value)
+        
+        changColor()
+        
     }
 
+    func changColor() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
   
     @IBAction func redSliderAction() {
+
+        let roundRedSliderValue = round(redSlider.value * 100) / 100.0
+        redValue.text = String(roundRedSliderValue)
+        changColor()
+        
     }
     
     @IBAction func greenSliderAction() {
+        let roundGreenSliderValue = round(greenSlider.value * 100) / 100.0
+        greenValue.text = String(roundGreenSliderValue)
+        changColor()
     }
     
     @IBAction func blueSliderAction() {
+        let roundBlueSliderValue = round(blueSlider.value * 100) / 100.0
+        blueValue.text = String(roundBlueSliderValue)
+        changColor()
     }
     
 }
