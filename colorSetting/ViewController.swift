742b19b19d3fb79ba78f11d917dc453e90ae909e
MarkEduardovich <321yamaha123@gmail.com>
2023-02-02 12:42:14 +0200

    initialCommit

---
+++ colorSetting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  colorSetting
+//
+//  Created by Mark on 02.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
